1455b08a7e83e6d6ac0b962fd78726bdb438804f
barabel <barabel324@yandex.ru>
2026-03-29 21:22:17 +0400

    @ -> src/frontend

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import '@/frontend/assets/styles';
+import App from './app/App.tsx';
+import '@/assets/styles';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
