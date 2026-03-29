fe5af86ef73d377f0e6b61bc1bd558c511202b60
barabel <barabel324@yandex.ru>
2026-03-29 20:24:41 +0400

    init

---
+++ src/electron/api.ts
@@ -0,0 +1,8 @@
+import { ipcMain } from "electron";
+
+ipcMain.handle(
+  "node-version",
+  () => {
+    return process.versions.node;
+  }
+);
---
+++ src/frontend/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [nodeVersion, setNodeVersion] = useState<string | undefined>(undefined);
+
+  const updateNodeVersion = async () => setNodeVersion(await backend.nodeVersion("Hello from App.tsx!"));
+
+
+  return (
+    <button onClick={updateNodeVersion}>
+      Node version is {nodeVersion}
+    </button>
+  )
+}
+
+export default App
