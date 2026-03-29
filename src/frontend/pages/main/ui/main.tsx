64a9ed6f99ed426f0b0644a7d2c1336f94f46311
barabel <barabel324@yandex.ru>
2026-03-29 22:04:34 +0400

    i18n

--- src/frontend/pages/main/ui/main.tsx
+++ src/frontend/pages/main/ui/main.tsx
@@ -1,15 +1,22 @@
+import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router';
 
 export const MainPage: FCClass = ({
   className,
 }) => {
+  const { t } = useTranslation();
+
   return (
     <div
       className={className}
     >
       <NavLink to="/recipe">
-        рецепты
+        {t('recipes')}
       </NavLink>
+
+      <div>
+        {t('test')}
+      </div>
     </div>
   );
 };
--- src/frontend/pages/recipe/ui/recipe.tsx
+++ src/frontend/pages/recipe/ui/recipe.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router';
 
 export const RecipePage: FCClass = ({
   className,
 }) => {
+  const { t } = useTranslation();
+
   return (
     <div
       className={className}
     >
       <NavLink to="/">
-        главная
+        {t('main')}
       </NavLink>
     </div>
   );
